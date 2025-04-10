f3be84a7b5212e02e4a0d90008f2845a6c8dbf40
MikelBarajas38 <mikelbarajas388@gmail.com>
2025-04-10 09:41:13 -0600

    fix linting

--- src/documents/views.py
+++ src/documents/views.py
@@ -3,7 +3,6 @@ Views for the documents API
 """
 
 from rest_framework import viewsets
-from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter
 
 from core.models import (
