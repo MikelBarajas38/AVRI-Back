56b8a661b6dd02189cb9cf15b13c24037339d13d
MikelBarajas38 <mikelbarajas388@gmail.com>
2025-09-22 15:43:57 -0400

    add ragflow service tests

--- src/core/services/ragflow_service.py
+++ src/core/services/ragflow_service.py
@@ -1,7 +1,6 @@
 import os
 import requests
 from typing import Dict, Any, List, Optional
-import json
 
 
 class RAGFlowService:
@@ -20,54 +19,8 @@ class RAGFlowService:
             'Content-Type': 'application/json',
             'Authorization': f'Bearer {self.api_key}' if self.api_key else None
         }
-        print(f'Base URL: {self.base_url}')
-        print(f'API Key: {self.api_key}')
-
-    def main(self):
-        """
-        Main method to run the service.
-        """
-        ragflow_service = RAGFlowService()
-
-        assistants = ragflow_service.list_assistants(name='AVRI')
-        print(json.dumps(assistants, indent=2))
-
-        name = assistants['data'][0]['name']
-        id = assistants['data'][0]['id']
-        print(f'Name: {name}, ID: {id}')
-
-        sessions = ragflow_service.list_sessions(assistant_id=id)
-        print(json.dumps(sessions, indent=2))
-        session_id_1 = sessions['data'][0]['id']
-        print(f'Session ID: {session_id_1}')
-
-        session = ragflow_service.create_session(
-            assistant_id=id,
-            session_name='My session'
-        )
-        print(json.dumps(session, indent=2))
-        session_id = session['data']['id']
-        print(f'Session ID: {session_id}')
-
-        response = ragflow_service.ask(
-            assistant_id=id,
-            question='Hello',
-            session_id=session_id,
-        )
-        print(json.dumps(response, indent=2))
-
-        response = ragflow_service.ask(
-            assistant_id=id,
-            question='Tell me about breast cancer',
-            session_id=session_id_1,
-        )
-        print(json.dumps(response, indent=2))
-
-        session = ragflow_service.delete_session(
-            assistant_id=id,
-            session_ids=[session_id]
-        )
-        print(json.dumps(session, indent=2))
+        # print(f'Base URL: {self.base_url}')
+        # print(f'API Key: {self.api_key}')
 
     def list_assistants(
             self,
