57006aebb5655cdc5bac0d8ce6277eb1c1d10ae0
juanQNav <juan.qn363@gmail.com>
2025-11-22 14:40:17 -0600

    fix: flake8 warnings

--- src/core/admin.py
+++ src/core/admin.py
@@ -120,7 +120,8 @@ class DocumentAdmin(admin.ModelAdmin):
     Define the admin pages for documents.
     """
     ordering = ['created_at']
-    list_display = ['title', 'id', 'repository_id', 'created_at', 'updated_at', 'status']
+    list_display = ['title', 'id', 'repository_id',
+                    'created_at', 'updated_at', 'status']
     list_filter = ['status']
     fieldsets = (
         (None, {'fields': ('id', 'title',)}),
